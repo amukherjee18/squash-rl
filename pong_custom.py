b647d16d4b3910fe35c020b8726f054a2f52aae3
amukherjee18 <arin.mukherjee.06@gmail.com>
2024-05-04 17:39:43 -0400

    rendering using trained model

--- pong_custom.py
+++ pong_custom.py
@@ -1,7 +1,55 @@
-import gym
-from gym import spaces
+import gymnasium as gym
+from gymnasium import spaces
 import numpy as np
 import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+
+def rgb_to_grayscale(rgb_tensor):
+    # Convert RGB tensor to grayscale using weighted average
+    grayscale_tensor = np.dot(rgb_tensor[..., :3], [0.2989, 0.5870, 0.1140])
+
+    return torch.FloatTensor(grayscale_tensor)
+
+
+class PolicyNetwork(nn.Module):
+
+  """
+    Implement the Policy Network.
+
+    Your task is to complete the initialization of the policy network that maps states to action probabilities.
+    This network should consist of several fully connected layers with ReLU activation, followed by a final layer
+    that outputs logits for each action. The forward pass should return a Categorical distribution over actions.
+
+    Instructions:
+    1. Initialize the fully connected layers in the __init__ method.
+    2. Implement the forward pass to return a Categorical distribution given state inputs.
+
+    Hint: The constructor takes 'state_dim' and 'action_dim' as arguments, representing the dimensions
+    of the state space and action space, respectively.
+  """
+  def __init__(self, state_dim, action_dim):
+      super(PolicyNetwork, self).__init__()
+      ##### Code implementation here #####
+      hidden=64
+      self.l1 = nn.Linear(state_dim, hidden)
+      self.l2 = nn.Linear(hidden, hidden)
+      self.l3 = nn.Linear(hidden, hidden)
+      self.output = nn.Linear(hidden, action_dim)
+      ############################
+
+  def forward(self, x):
+      ##### Code implementation here #####
+      layer1 = F.relu(self.l1(x))
+      layer2 = F.relu(self.l2(layer1))
+      layer3 = F.relu(self.l3(layer2))
+      output_logits = self.output(layer3)
+      dist = Categorical(logits=output_logits)
+      return dist
+
 
 class CustomPongEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -25,7 +73,7 @@ class CustomPongEnv(gym.Env):
 
         # Game settings
         self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
-        self.ball_velocity = [4, 2]
+        self.ball_velocity = [4, np.random.choice([-1, 1])*2]
         self.ball_size = 10
         self.paddle_position = int(self.front_wall_length / 2)
         self.paddle_width = 80
@@ -46,10 +94,20 @@ class CustomPongEnv(gym.Env):
 
     def reset(self):
         self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
-        self.ball_velocity = [4, 2]
+        self.ball_velocity = [4, np.random.choice([-1, 1])*2]
+        self.ball_size = 10
         self.paddle_position = int(self.front_wall_length / 2)
+        self.paddle_width = 80
+        self.paddle_height = 10
+        # self.score = 0
 
-        self.score = 0
+        self.done = False
+        
+        # self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
+        # self.ball_velocity = [4, np.random.choice([-1, 1])*2]
+        # self.paddle_position = int(self.front_wall_length / 2)
+
+        # self.score = 0
         return self._get_obs()
 
     def render(self, mode='human', close=False):
@@ -116,7 +174,7 @@ class CustomPongEnv(gym.Env):
             self.score += 1
             self.ball_position = [105, 80]
             self.ball_velocity = [4, -2 if self.ball_velocity[1] > 0 else 2] 
-            # self.done = True
+            self.done = True
 
     def _get_reward(self):
         if self.ball_position[0] > self.side_wall_length + self.edge_width:
@@ -136,16 +194,46 @@ class CustomPongEnv(gym.Env):
         return obs
 
 def test_pong_environment(episodes=10):
+    
     # Create the environment
-    env = CustomPongEnv()
+    env = CustomPongEnv() 
+    
+    state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
+    action_dim = env.action_space.n
+
+    policy_net = PolicyNetwork(state_dim, action_dim)
+    # checkpoint = torch.load('ppo_step101', map_location=torch.device('cpu'))
+    # policy_net.load_state_dict(checkpoint['policy network'])
+    # print(policy_net)
+    score = 0
+    
     for episode in range(episodes):
+        # breakpoint()
         done = False
-        score = 0
+        
         obs = env.reset()
         while not done:
             # Random action
-            action = env.action_space.sample()
+            # action = env.action_space.sample()
+
+            obs = torch.FloatTensor(obs)
+            obs = rgb_to_grayscale(obs).flatten()   
+            
+                 
+            # breakpoint()
+            # Action according to trained policy
+            action = policy_net(obs).sample().numpy()
+            # breakpoint()
+            # action = action[0]
+
+
             obs, reward, done, info = env.step(action)
+
+
+            # obs = rgb_to_grayscale(obs).flatten()
+            # obs = torch.FloatTensor(obs)
+
+
             score += reward
             # Render the game
             env.render()
