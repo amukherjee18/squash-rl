aca53a904ee1a96de4ccc656f312847dafac5b06
amukherjee18 <arin.mukherjee.06@gmail.com>
2024-05-05 17:44:40 -0400

    got rid of edge_width completely, fixed edge constraints on paddle motion

--- pong_custom.py
+++ pong_custom.py
@@ -57,28 +57,33 @@ class CustomPongEnv(gym.Env):
     def __init__(self):
         super(CustomPongEnv, self).__init__()
 
-        # Court measurements
-        self.scale = 10
+        # Measurements
+        self.scale = 20
+
+        # Court
         self.front_wall_length = 21 * self.scale
         self.side_wall_length = 32 * self.scale
         self.front_wall_to_t = int(17.85 * self.scale)
         self.box_side_length = int(5.25 * self.scale)
         self.t_to_back_wall = self.side_wall_length - self.front_wall_to_t
+        self.line_width = int(0.2*self.scale)
 
-        # Rendering measurements, colors
-        self.edge_width = 2 #int(0.5*self.scale)
-        
-        self.action_space = spaces.Discrete(3)  # 0: stay, 1: up, 2: down
-        self.observation_space = spaces.Box(low=0, high=255, shape=(self.side_wall_length, self.front_wall_length, 3), dtype=np.uint8)
-
-        # Game settings
+        # Ball
         self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
         self.ball_velocity = [int(2*self.scale / 10), int(np.random.choice([-1, 1])*self.scale / 10)]
-        self.ball_size = self.scale
-        self.paddle_position = int(self.front_wall_length / 2)
-        self.paddle_width = 80
-        self.paddle_height = 10
-        self.score = 0
+        self.ball_radius = int(self.scale / 2)
+        self.ball_size = int(self.scale / 2) # For state sent to policy net
+        self.dot_radius = int(self.scale / 10)
+
+        # Paddle
+        self.paddle_center = int(self.front_wall_length / 2)
+        self.paddle_halfwidth = int(self.front_wall_length / 12)
+        self.paddle_height = int(self.scale/2)
+        self.paddle_velocity = 2
+
+        # State, Action Spaces
+        self.action_space = spaces.Discrete(3)  # 0: stay, 1: up, 2: down
+        self.observation_space = spaces.Box(low=0, high=255, shape=(self.side_wall_length, self.front_wall_length, 3), dtype=np.uint8)
 
         self.done = False
 
@@ -88,60 +93,56 @@ class CustomPongEnv(gym.Env):
 
         reward = self._get_reward()
         done = self._is_done()
-        info = {'score': self.score}
 
-        return self._get_obs(), reward, done, info
+        return self._get_obs(), reward, done
 
     def reset(self):
         self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
         self.ball_velocity = [int(2*self.scale / 10), int(np.random.choice([-1, 1])*self.scale / 10)]
-
-        self.ball_size = 10
-        self.paddle_position = int(self.front_wall_length / 2)
-        self.paddle_width = 80
-        self.paddle_height = 10
-        # self.score = 0
+        self.paddle_center = int(self.front_wall_length / 2)
 
         self.done = False
         
-        # self.ball_position = [int(self.side_wall_length/2), int(self.front_wall_length/2)]
-        # self.ball_velocity = [4, np.random.choice([-1, 1])*2]
-        # self.paddle_position = int(self.front_wall_length / 2)
-
-        # self.score = 0
         return self._get_obs(), []
 
     def render(self, mode='human', close=False):
         
         # Screen dimensions
-        screen = np.zeros((self.side_wall_length + 2*self.edge_width, self.front_wall_length + 2*self.edge_width, 3), dtype=np.uint8)
-
-        # Right Wall
-        screen[self.side_wall_length + self.edge_width:self.side_wall_length + 2*self.edge_width, :] = [255, 255, 255]
-        # Left Wall
-        screen[:, 0:self.edge_width] = [255, 255, 255]
-        # Front Wall
-        screen[0:self.edge_width, :]
-        # Back Wall
-        screen[:, self.front_wall_length + self.edge_width:self.front_wall_length + 2*self.edge_width] = [255, 255, 255]
+        screen = np.zeros((self.side_wall_length, self.front_wall_length, 3), dtype=np.uint8)
+        screen[:,:] = [149, 203, 231]
 
         # T
-        screen[self.front_wall_to_t: self.front_wall_to_t + self.edge_width, 0: self.front_wall_length + 2*self.edge_width] = [255,255,255]
-        screen[self.front_wall_to_t:self.side_wall_length + 2*self.edge_width , int((self.front_wall_length + 2*self.edge_width - self.edge_width)/2):int((self.front_wall_length+ 2*self.edge_width + self.edge_width)/2)] = [255,255,255]
+        screen[self.front_wall_to_t: self.front_wall_to_t + self.line_width , 0: self.front_wall_length] = [0,0,255]
+        screen[self.front_wall_to_t:self.side_wall_length + 2*self.line_width , int((self.front_wall_length - self.line_width)/2):int((self.front_wall_length + self.line_width)/2)] = [0,0,255]
 
         # Boxes
-        screen[self.front_wall_to_t + self.edge_width + self.box_side_length: self.front_wall_to_t + 2*self.edge_width + self.box_side_length, self.edge_width: self.edge_width + self.box_side_length] = [255,255,255]
-        screen[self.front_wall_to_t + self.edge_width + self.box_side_length: self.front_wall_to_t + 2*self.edge_width + self.box_side_length, self.front_wall_length + self.edge_width - self.box_side_length: self.front_wall_length + self.edge_width] = [255,255,255]
+        # Horizontal Lines
+        screen[self.front_wall_to_t + self.line_width + self.box_side_length: self.front_wall_to_t + 2*self.line_width + self.box_side_length, 0:self.box_side_length] = [0,0,255]
+        screen[self.front_wall_to_t + self.line_width + self.box_side_length: self.front_wall_to_t + 2*self.line_width + self.box_side_length, self.front_wall_length - self.box_side_length: self.front_wall_length] = [0,0,255]
+        # Vertical Lines
+        screen[self.front_wall_to_t + self.line_width: self.front_wall_to_t + 2*self.line_width + self.box_side_length, self.box_side_length:self.line_width + self.box_side_length] = [0,0,255]
+        screen[self.front_wall_to_t + self.line_width: self.front_wall_to_t + 2*self.line_width + self.box_side_length, self.front_wall_length - self.box_side_length: self.front_wall_length + self.line_width - self.box_side_length] = [0,0,255]
 
-        screen[self.front_wall_to_t + self.edge_width: self.front_wall_to_t + 2*self.edge_width + self.box_side_length, self.box_side_length + self.edge_width:2*self.edge_width + self.box_side_length] = [255,255,255]
-        screen[self.front_wall_to_t + self.edge_width: self.front_wall_to_t + 2*self.edge_width + self.box_side_length, self.front_wall_length - self.box_side_length: self.front_wall_length + self.edge_width - self.box_side_length] = [255,255,255]
-
+        # Function to draw a filled circle in the numpy array
+        def draw_circle(image, center, radius, color):
+            # Generate array of coordinates for the circle
+            rows, cols = np.ogrid[:image.shape[0], :image.shape[1]]
+            circle_mask = (rows - center[0])**2 + (cols - center[1])**2 <= radius**2
+            # Draw the circle on the image
+            image[circle_mask] = color
 
 
         # Draw the ball
-        screen[self.ball_position[0]-self.ball_size:self.ball_position[0]+self.ball_size, self.ball_position[1]-self.ball_size:self.ball_position[1]+self.ball_size] = [255, 0, 0]
+        ball_center = (self.ball_position[0], self.ball_position[1])
+        ball_left_dot_center = (self.ball_position[0] - int(self.ball_radius/4), self.ball_position[1] - int(self.ball_radius/4))
+        ball_right_dot_center = (self.ball_position[0] - int(self.ball_radius/4), self.ball_position[1] + int(self.ball_radius/4))
+        draw_circle(screen, ball_center, self.ball_radius, [0, 0, 0])
+        draw_circle(screen, ball_left_dot_center, self.dot_radius, [0, 255, 255])
+        draw_circle(screen, ball_right_dot_center, self.dot_radius, [0, 255, 255])
+
         # Draw the paddle
-        screen[self.side_wall_length:self.side_wall_length+self.paddle_height, self.paddle_position: self.paddle_position + self.paddle_width] = [0, 0, 255]
+        screen[self.side_wall_length - self.paddle_height:self.side_wall_length, self.paddle_center - self.paddle_halfwidth: self.paddle_center + self.paddle_halfwidth] = [0, 0, 0]
+
 
         if mode == 'human':
             import cv2
@@ -149,54 +150,53 @@ class CustomPongEnv(gym.Env):
             cv2.waitKey(1)
 
     def _take_action(self, action):
-        if action == 1 and self.paddle_position > 0:
-            self.paddle_position -= 4  # Move paddle left
-        elif action == 2 and self.paddle_position < 160:
-            self.paddle_position += 4  # Move paddle right
+        if action == 1 and self.paddle_center - self.paddle_halfwidth > 0:
+            self.paddle_center -= self.paddle_velocity  # Move paddle left
+        elif action == 2 and self.paddle_center + self.paddle_halfwidth < self.front_wall_length:
+            self.paddle_center += self.paddle_velocity # Move paddle right
 
     def _update_ball(self):
         self.ball_position[0] += self.ball_velocity[0]
         self.ball_position[1] += self.ball_velocity[1]
 
         # Check for collision with front wall
-        if self.ball_position[0] <= self.edge_width:
+        if self.ball_position[0] - self.ball_radius <= 0:
             self.ball_velocity[0] = -self.ball_velocity[0]
 
         # Check for collision with the side walls
-        if self.ball_position[1] <= self.edge_width or self.ball_position[1] >= self.front_wall_length + self.edge_width:
+        if self.ball_position[1] - self.ball_radius <= 0 or self.ball_position[1] + self.ball_radius >= self.front_wall_length:
             self.ball_velocity[1] = -self.ball_velocity[1]
 
         # Check for collision with the paddle
-        if self.ball_position[0] >= self.edge_width + self.side_wall_length - self.paddle_height and self.ball_position[1] >= self.paddle_position and self.ball_position[1] <= self.paddle_position + self.paddle_width:
+        if self.ball_position[0] + self.ball_radius >= self.side_wall_length - self.paddle_height and self.ball_position[1] >= self.paddle_center - self.paddle_halfwidth and self.ball_position[1] <= self.paddle_center + self.paddle_halfwidth:
             self.ball_velocity[0] = -self.ball_velocity[0]
 
         # Check for scoring
-        if self.ball_position[0] >= self.side_wall_length + 2*self.edge_width:
-            self.score += 1
-            self.ball_position = [105, 80]
-            self.ball_velocity = [int(2*self.scale / 10), int(np.random.choice([-1, 1])*self.scale / 10)]
+        if self.ball_position[0] + self.ball_radius >= self.side_wall_length:
+            
+            # Reset
+            # self.ball_position = [105, 80]
+            # self.ball_velocity = [int(2*self.scale / 10), int(np.random.choice([-1, 1])*self.scale / 10)]
 
             # self.ball_velocity = [4, -2 if self.ball_velocity[1] > 0 else 2] 
             self.done = True
 
     def _get_reward(self):
-        if self.ball_position[0] > self.side_wall_length + self.edge_width:
+        if self.ball_position[0] + self.ball_radius >= self.side_wall_length:
             return -10
         # Check for collision with the paddle
-        if self.ball_position[0] >= self.edge_width + self.side_wall_length - self.paddle_height and self.ball_position[1] >= self.paddle_position and self.ball_position[1] <= self.paddle_position + self.paddle_width:
+        if self.ball_position[0] + self.ball_radius >= self.side_wall_length - self.paddle_height and self.ball_position[1] >= self.paddle_center - self.paddle_halfwidth and self.ball_position[1] <= self.paddle_center + self.paddle_halfwidth:
             return 5
         return 0
 
     def _is_done(self):
-        # return False
         return self.done
-
 
     # possible there is some issue with ball_position in rendering (w borders and stuff) vs ball_position that agent gets 
     def _get_obs(self):
         obs = np.zeros((self.side_wall_length, self.front_wall_length, 3), dtype=np.uint8)
         obs[self.ball_position[0]-self.ball_size:self.ball_position[0]+self.ball_size, self.ball_position[1]-self.ball_size:self.ball_position[1]+self.ball_size] = 255
-        obs[self.edge_width + self.side_wall_length - self.paddle_height - 1:self.edge_width + self.side_wall_length - self.paddle_height + 1, self.paddle_position:self.paddle_position+self.paddle_width] = 255
+        obs[self.side_wall_length - self.paddle_height - 1:self.side_wall_length - self.paddle_height + 1, self.paddle_center - self.paddle_halfwidth:self.paddle_center+self.paddle_halfwidth] = 255
         return obs
 
 def test_pong_environment(episodes=10):
@@ -204,53 +204,34 @@ def test_pong_environment(episodes=10):
     # Create the environment
     env = CustomPongEnv() 
     
-    state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
-    action_dim = env.action_space.n
-
-    policy_net = PolicyNetwork(state_dim, action_dim)
-    # checkpoint = torch.load('ppo_step301', map_location=torch.device('cpu'))
-    checkpoint = torch.load('ppo_collisionreward_step2501', map_location=torch.device('cpu'))
-    
+    # Fetch policy net params
+    # state_dim = env.observation_space.shape[0] * env.observation_space.shape[1]
+    # action_dim = env.action_space.n
+    # policy_net = PolicyNetwork(state_dim, action_dim)
+    # checkpoint = torch.load('ppo_collisionreward_step2501', map_location=torch.device('cpu'))
     # policy_net.load_state_dict(checkpoint['policy network'])
-    # print(policy_net)
-    score = 0
     
     for episode in range(episodes):
-        # breakpoint()
         done = False
+        reward_sum = 0
         
         obs, _ = env.reset()
         while not done:
-            # Random action
-            # action = env.action_space.sample()
-
             obs = torch.FloatTensor(obs)
             obs = rgb_to_grayscale(obs).flatten()   
-            
-                 
-            # breakpoint()
+
             # Action according to trained policy
-            action = policy_net(obs).sample().numpy()
-            # breakpoint()
-            # action = action[0]
+            # action = policy_net(obs).sample().numpy()
+            # Random Action
+            action = env.action_space.sample()
 
+            obs, reward, done = env.step(action)
+            reward_sum += reward
 
-            obs, reward, done, info = env.step(action)
-
-
-            # obs = rgb_to_grayscale(obs).flatten()
-            # obs = torch.FloatTensor(obs)
-
-
-            score += reward
             # Render the game
             env.render()
 
-            # You can print observations, rewards, and info if you want to see details
-            # print(f"Observation: {obs}")
-            # print(f"Reward: {reward}, Info: {info}")
-
-        print(f"Episode {episode + 1}: Score = {info['score']}")
+        print(f"Episode {episode + 1}: Score = {reward_sum}")
 
     env.close()
 
